0185c2562aeafdcfb7cd33c76f77b4e133890413
Marcelinhoj <m_seoy@msn.com>
2022-09-28 22:24:38 -0300

    1

--- MySQL/001.sql
+++ MySQL/001.sql
@@ -1,13 +1,16 @@
 CREATE DATABASE cadastro;
 default character utf8,
 use cadastro;
+
 create table pessoas (
+id int not null auto_increment,
 nome varchar(30) no null,
 idade date,
 sexo enum('M','F'),
 peso decimal(5,2),
-altura float,
-nacionalidade varchar(20)
+altura decimal(3,2),
+nacionalidade varchar(20) default 'Brsil',
+primary key (id)
 
 );
 
